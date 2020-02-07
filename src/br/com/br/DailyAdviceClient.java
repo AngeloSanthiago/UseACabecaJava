56d43e2af70ba2fd8a6f13a595cf85d70066c0e1
angel <angel@DESKTOP-27E4LQM>
2020-02-07 19:43:39 -0300

    Cap15_CrieUmaConexão-Pag.340 - Cliente inicia conexão com o servidor

--- src/br/com/br/DailyAdviceClient.java
+++ src/br/com/br/DailyAdviceClient.java
@@ -1,5 +1,37 @@
 package br.com.br;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 public class DailyAdviceClient {
+	public void go() {
+		try {
+			// Estabelece conexão com um servidor
+			Socket s = new Socket("127.0.0.01", 4242);
+			
+//			Para LEITURA DO SERVIDOR cria-se um objeto de LEITURA que recebe um objeto de SAIDA
+//			de DADOS DO SERVIDOR
+			InputStreamReader inputStreamReader = new InputStreamReader(s.getInputStream());
+			
+			BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+			String advice = bufferedReader.readLine();
+			
+			System.out.println("Today you should: "+ advice);
+			
+			bufferedReader.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+//			System.out.println(e);
+			
+		}
+		
+	}
+	public static void main(String[] args) {
+		DailyAdviceClient dailyAdviceClient = new DailyAdviceClient();
+		dailyAdviceClient.go();
+	}
 
 }
